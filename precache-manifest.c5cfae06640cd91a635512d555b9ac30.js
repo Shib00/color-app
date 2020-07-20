f9e117ef62b3d5d716f6c90ddb7ef1dd9d8bb80a
shivam.dwivedi <shivam.dwivedi@innovaccer.com>
2020-07-20 10:58:28 +0530

    Updates

---
+++ precache-manifest.c5cfae06640cd91a635512d555b9ac30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144ca5ef602d74b12f3d52834bbf78df",
+    "url": "/color-app/index.html"
+  },
+  {
+    "revision": "2bf4cc236a4881db5b51",
+    "url": "/color-app/static/css/2.c687f723.chunk.css"
+  },
+  {
+    "revision": "79c99c145c32a0618745",
+    "url": "/color-app/static/css/main.636bdef3.chunk.css"
+  },
+  {
+    "revision": "2bf4cc236a4881db5b51",
+    "url": "/color-app/static/js/2.429c9569.chunk.js"
+  },
+  {
+    "revision": "79c99c145c32a0618745",
+    "url": "/color-app/static/js/main.fa1f7e3f.chunk.js"
+  },
+  {
+    "revision": "ea958c06051b899540ac",
+    "url": "/color-app/static/js/runtime~main.bf85e199.js"
+  }
+]);
